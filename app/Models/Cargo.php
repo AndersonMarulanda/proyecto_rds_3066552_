66b9c79efa349cb434f5a0fa6776786d911cc7cf
AndersonMarulanda <andersonmarulanda15@gmail.com>
2026-06-15 19:36:18 -0500

    feat: configurar modelos y relaciones

--- app/Models/Cargo.php
+++ app/Models/Cargo.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cargo extends Model
 {
+    use HasFactory;
+
     protected $table = 'cargo';
 
     protected $primaryKey = 'id_cargo';
--- app/Models/FuncionCargo.php
+++ app/Models/FuncionCargo.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FuncionCargo extends Model
 {
+    use HasFactory;
+
     protected $table = 'funciones_cargo';
 
     protected $primaryKey = 'id_funcion';
